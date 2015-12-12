d553649c27261d7f6d32c89c8e2e6008a8fdcfa5
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-12-11 17:18:46 -0800

    models for user and workout

---
+++ db/migrate/20151212010724_create_workouts.rb
@@ -0,0 +1,16 @@
+class CreateWorkouts < ActiveRecord::Migration
+  def change
+    create_table :workouts do |t|
+      t.references :user, index: true, foreign_key: true
+      t.string :type
+      t.string :name
+      t.integer :set_amount
+      t.integer :weight
+      t.time :avg_time
+      t.datetime :weekday
+      t.boolean :weekly
+
+      t.timestamps null: false
+    end
+  end
+end
