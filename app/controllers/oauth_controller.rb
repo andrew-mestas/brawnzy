a636b5e3c133d82546629300418faf749eef2165
Yahya Hussein <yahyahussein91@gmail.com>
2015-12-13 12:36:15 -0800

    oauth works just need local validation

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,14 +3,14 @@ class OauthController < ApplicationController
   	provider_user = request.env['omniauth.auth']
 
   	# find or create a user
-  	user = User.find_or_create_by(provider_id: provider_user['uid'], provider: params[:params]) do |u|
+  	user = User.find_or_create_by(provider_id: provider_user['uid'], provider: params[:provider]) do |u|
   		u.provider_hash = provider_user['credentials']['token']
   		u.name = provider_user['info']['name']
   		u.email = provider_user['info']['email']
-  		u.password = u.password_confirmation = SecureRandom.urlsafe_base64(n=6)
+  		# u.password = u.password_confirmation = SecureRandom.urlsafe_base64(n=6)
   	end
   	#store user inside of session
-  	session[:user_id] = user.user_id
+  	session[:user_id] = user_id
   	redirect_to root_path
   end
 
