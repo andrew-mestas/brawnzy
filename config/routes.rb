0880011d57c390644fe664e1576046634b42e516
Yahya Hussein <yahyahussein91@gmail.com>
2015-12-13 11:54:41 -0800

    still setting up oauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   # post 'user/new' 
 
+  # provider routes for facebook/auth controller
+  get 'auth/logout' => 'oauth#logout'
+  get 'auth/failure' => 'oauth#failure'
+  get 'auth/:provider/callback' => 'oauth#callback'
 
+  # routes for main app
   get 'workout/all' => 'workout#all'
 
   get 'workout/new' => 'workout#new'
