6fb183a1da5f4db5011b79c84a521ee5f01974b0
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-12-17 11:43:22 -0800

    fix heroku

--- app/controllers/userapi_controller.rb
+++ app/controllers/userapi_controller.rb
@@ -3,25 +3,25 @@ class UserapiController < ApplicationController
 	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
  
   def create 
-	# tempuser = User.find_by_email(user_params[:email])
-	# 		if tempuser
-	# 			render json: {msg: "User Exists", status: "Error"}
-	# 		else
-	# 			@user = User.create(name: user_params[:name],
-	# 								email: user_params[:email], 
-	# 								password: user_params[:clientID],
-	# 								password_confirmation: user_params[:clientID])
-	# 			if @user
-	# 				# session[:user_id] = @user.id
-	# 				# flash[:success] = "User created!!"
-	# 				# redirect_to root_path
-	# 				render json: {msg: "Created User", status: "OK", token: params}
-	# 			else
-	# 				render json: {msg: "Credentials Invalid!!", status: "Error"}
-	# 				# redirect_to '/users/new'
-	# 			end
-	# 		end  
-	render json: user_params
+	tempuser = User.find_by_email(user_params[:email])
+			if tempuser
+				render json: {msg: "User Exists", status: "Error"}
+			else
+				@user = User.create(name: user_params[:name],
+									email: user_params[:email], 
+									password: user_params[:clientID],
+									password_confirmation: user_params[:clientID])
+				if @user
+					# session[:user_id] = @user.id
+					# flash[:success] = "User created!!"
+					# redirect_to root_path
+					render json: {msg: "Created User", status: "OK", token: params}
+				else
+					render json: {msg: "Credentials Invalid!!", status: "Error"}
+					# redirect_to '/users/new'
+				end
+			end  
+	# render json: user_params
 	end
 	
 	private
