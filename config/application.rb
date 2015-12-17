c9b873428dfdfdb2d2f2638341dfed6e161d8b53
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-12-16 21:35:19 -0800

    heroku stuff

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 
 module Brawnzy
   class Application < Rails::Application
-    config.web_console.whitelisted_ips = '10.11.98.58'
+    # config.web_console.whitelisted_ips = '10.11.98.58'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
